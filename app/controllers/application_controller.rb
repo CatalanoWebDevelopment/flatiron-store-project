529abfd0d3115d67ef85607e647a57c6bce85301
catalanowebdevelopment <catalano.work@gmail.com>
2018-04-09 15:57:32 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,48 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
     
+    # This is a page full of comments
+        # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+    # This is a page full of comments
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,3 @@
 class Order < ActiveRecord::Base
     belongs_to :user
-    
-    def change
-        "This changes my code from being like githubs."
-    end
 end
